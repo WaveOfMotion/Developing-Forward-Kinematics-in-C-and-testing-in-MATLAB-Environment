1ad185bf39afe34b3c552ead15a2483f9ce27f2d
Toms Timšans <waveofmotion2@gmail.com>
2026-06-30 21:03:30 +0300

    Delete forward_kinematics.h

--- forward_kinematics.h
+++
@@ -1,8 +0,0 @@
-#ifndef FORWARD_KINEMATICS
-#define FORWARD_KINEMATICS
-
-#include "matrix_operations.h"
-
-void forward_kinematics(double jointA1, double jointA2, double jointA3, double jointA4, double jointA5, double jointA6, Matrix_4x4 *result);
-
-#endif
